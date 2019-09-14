0ae4c63c64c1a67cf4f114feb71aedd5f85ada16
OlexandrSkomarovsky <olex79if@gmail.com>
2019-09-14 10:23:24 +0300

    add task_1

---
+++ task_1.py
@@ -0,0 +1,14 @@
+"""
+Define variables a and b. Read values a and b from console and calculate:
+a + b
+a - b 
+a * b 
+a / b.
+Output obtained results
+"""
+a = input("a: ")
+b = input("b: ")
+print(int(a)+int(b))
+print(int(a)-int(b))
+print(int(a)*int(b))
+print(int(a)/int(b))
